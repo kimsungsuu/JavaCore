087b4e155d4d5bb3a66b3a9d8895e4a5438814fb
ksungsu <zxzxzlxlzlxl@naver.com>
2022-11-06 23:14:48 +0900

    only java project

---
+++ src/test/java/com/sungsu/JavaCore/JavaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungsu.JavaCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
