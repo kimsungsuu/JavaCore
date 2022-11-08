982d14a280d84b021e979c4d138362f8931a3722
ksungsu <zxzxzlxlzlxl@naver.com>
2022-11-08 19:32:17 +0900

    use Spring AnnotationConfigApplicationContext AppConfig

--- src/main/java/com/sungsu/JavaCore/AppConfig.java
+++ src/main/java/com/sungsu/JavaCore/AppConfig.java
@@ -9,23 +9,30 @@ import com.sungsu.JavaCore.member.MemberServiceImpl;
 import com.sungsu.JavaCore.member.MemoryMemberRepository;
 import com.sungsu.JavaCore.order.OrderService;
 import com.sungsu.JavaCore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
         // 내가 만든 MemberServiceImpl은 MemoryMemberRepository를 사용할거야.라고 해석하면 편하다.
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- src/main/java/com/sungsu/JavaCore/OrderApp.java
+++ src/main/java/com/sungsu/JavaCore/OrderApp.java
@@ -7,15 +7,17 @@ import com.sungsu.JavaCore.member.MemberServiceImpl;
 import com.sungsu.JavaCore.order.Order;
 import com.sungsu.JavaCore.order.OrderService;
 import com.sungsu.JavaCore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
 
