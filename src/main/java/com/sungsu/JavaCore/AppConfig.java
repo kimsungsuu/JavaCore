0eb6c64b9e2c8c71517d13b2130d78ed38bdd338
ksungsu <zxzxzlxlzlxl@naver.com>
2022-11-08 17:25:24 +0900

    core java Study DI and OCP AppConfig

--- src/main/java/com/sungsu/JavaCore/AppConfig.java
+++ src/main/java/com/sungsu/JavaCore/AppConfig.java
@@ -1,6 +1,9 @@
 package com.sungsu.JavaCore;
 
+import com.sungsu.JavaCore.discount.DiscountPolicy;
 import com.sungsu.JavaCore.discount.FixDiscountPolicy;
+import com.sungsu.JavaCore.discount.RateDiscountPolicy;
+import com.sungsu.JavaCore.member.MemberRepository;
 import com.sungsu.JavaCore.member.MemberService;
 import com.sungsu.JavaCore.member.MemberServiceImpl;
 import com.sungsu.JavaCore.member.MemoryMemberRepository;
@@ -10,11 +13,20 @@ import com.sungsu.JavaCore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
         // 내가 만든 MemberServiceImpl은 MemoryMemberRepository를 사용할거야.라고 해석하면 편하다.
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
